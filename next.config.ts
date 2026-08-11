c3d9ea118c15f8f61046091bd37b05cedc3bbbf5
Tek Dakwak <118746617+patriarchpam@users.noreply.github.com>
2026-08-11 13:43:57 +0100

    Remove devIndicators to fix build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    buildActivity: false,
-  },
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
